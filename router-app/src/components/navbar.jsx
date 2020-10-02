49b456db9cedc912748f2073809b556e07e60d95
mhdali93 <41387912+mhdali93@users.noreply.github.com>
2020-10-02 07:08:02 +0530

    mosh solved commit

---
+++ router-app/src/components/navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/products">Products</Link>
+      </li>
+      <li>
+        <Link href="/posts/2018/06">Posts</Link>
+      </li>
+      <li>
+        <Link href="/admin">Admin</Link>
+      </li>
+    </ul>
+  );
+};
+
+export default NavBar;
